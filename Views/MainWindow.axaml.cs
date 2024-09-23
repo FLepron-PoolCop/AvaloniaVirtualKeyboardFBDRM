9eedc15f84df1db598077deef8704a811f6f6bbc
FLepron-PoolCop <francois.lepron@poolcop.com>
2024-09-23 09:20:47 +0200

    first commit

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaVirtualKeyboardFBDRM.Views;
+
+public partial class MainWindow : UserControl
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
